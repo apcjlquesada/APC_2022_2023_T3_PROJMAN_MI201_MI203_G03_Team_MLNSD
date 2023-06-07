a4f9da2f2c28add476a37b0fa8af530ec98a09a6
NatSison <111641301+NatSison@users.noreply.github.com>
2023-06-08 03:57:30 +0800

    Modify index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../app/bootstrap.php';
+require_once 'C:/wamp64/www/APC_2022_2023_T3_PROJMAN_MI201_MI203_G03_Team_MLNSD/app/bootstrap.php';
 
 // Init Core Library
 $init = new Core;
